f83acb635c9baa1ce90cb130e4bca92bc5a2f112
Gazi HATAŞ <gazihatas71@gmail.com>
2022-02-18 05:30:05 +0300

    Veritabanı hazırlandı ve Bağlantı işlemleri yapıldı.

---
+++ database/migrations/2022_02_18_021923_create_appointments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullName');
+            $table->string('phone');
+            $table->string('email');
+            $table->date('date');
+            $table->integer('workingHour');
+            $table->integer('notification_type')->default(0);
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
